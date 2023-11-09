af30fa7189b951754b963ae719b681c551577add
unimafy <nima.yonten1729@gmail.com>
2023-11-09 17:51:00 +0600

    add user creation api

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { DbModule, LoggerModule } from '@app/common';
+import { UserDocument, UserSchema } from './models/user.schema';
+import { UsersRepository } from './users.repository';
 
 @Module({
-  controllers: [UsersController]
+  imports: [
+    DbModule,
+    DbModule.forFeature(
+      [{ name: UserDocument.name, schema: UserSchema }]
+    ),
+   LoggerModule
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository]
 })
 export class UsersModule {}
---
+++ apps/auth/src/users/users.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly userRepository: UsersRepository) { }
+
+  create(createUserDto: CreateUserDto) {
+    return this.userRepository.create({
+      ...createUserDto,
+      timestamp: new Date()
+    });
+  }
+}
